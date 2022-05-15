7c3e14f5482e2b97b393082c9be57e2b5c0bb016
Chriszao <chris.f.assis18@gmail.com>
2022-05-15 10:43:49 -0300

    feat: Add fetchUsers and getUserById function handler

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func main() {
 	// Create
 	router.HandleFunc("/users", routes.InsertUser).Methods(http.MethodPost)
 
+	// ReadAll
+	router.HandleFunc("/users", routes.FetchUsers).Methods(http.MethodGet)
+
+	// ReadById
+	router.HandleFunc("/users/{id}", routes.GetUserById).Methods(http.MethodGet)
+
 	fmt.Println("✨ Listening on http://localhost:5000")
 
 	log.Fatal(http.ListenAndServe(":5000", router))
--- routes/routes.go
+++ routes/routes.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type user struct {
@@ -64,4 +67,89 @@ func InsertUser(writer http.ResponseWriter, request *http.Request) {
 
 	writer.WriteHeader(http.StatusCreated)
 	writer.Write([]byte(fmt.Sprintf("User successfully inserted! Id: %d", insertedId)))
+}
+
+func FetchUsers(writer http.ResponseWriter, request *http.Request) {
+	db, err := database.Connect()
+
+	if err != nil {
+		writer.Write([]byte("Error connecting to database!"))
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		writer.Write([]byte("Error fetching users!"))
+		return
+	}
+	defer rows.Close()
+
+	var users []user
+
+	for rows.Next() {
+		var user user
+
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			writer.Write([]byte("Error scanning user!"))
+			return
+		}
+		users = append(users, user)
+	}
+
+	writer.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(writer).Encode(users); err != nil {
+		writer.Write([]byte("Error encoding users to JSON"))
+		return
+	}
+}
+
+func GetUserById(writer http.ResponseWriter, request *http.Request) {
+	params := mux.Vars(request)
+
+	ID, err := strconv.ParseUint(params["id"], 10, 32)
+
+	if err != nil {
+		writer.Write([]byte("Error converting parameter to integer!"))
+		return
+	}
+
+	db, err := database.Connect()
+
+	if err != nil {
+		writer.Write([]byte("Error connecting to database!"))
+		return
+	}
+	defer db.Close()
+
+	row, err := db.Query("SELECT * FROM user WHERE id = ?", ID)
+
+	if err != nil {
+		writer.Write([]byte("Error getting user!"))
+		return
+	}
+	defer row.Close()
+
+	var user user
+
+	if row.Next() {
+		if err := row.Scan(&user.ID, &user.Name, &user.Email); err != nil {
+			writer.Write([]byte("Error scanning user!"))
+			return
+		}
+	}
+
+	if user.ID == 0 {
+		writer.WriteHeader(http.StatusNotFound)
+		writer.Write([]byte(fmt.Sprintf("User with id %d not found!", ID)))
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+
+	if err := json.NewEncoder(writer).Encode(user); err != nil {
+		writer.Write([]byte("Error encoding user to JSON"))
+		return
+	}
 }
